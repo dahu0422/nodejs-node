36d0b78445ba832ee2b8ed4b2cbd9ea5c06cf0d6
Dahu-0712 <419460476@qq.com>
2023-11-22 19:40:14 +0800

    feat：初始化仓库，读写文件、创建server。

---
+++ 1-node-farm/index.js
@@ -0,0 +1,39 @@
+const fs = require('fs');
+const http = require('http')
+
+// ----------------- 文件API -----------------
+// 阻塞、同步方式
+// const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
+// console.log(textIn);
+
+// const textOut = `This is what we know about the avocado: ${textIn}.\n Created on ${Date.now()}`
+// fs.writeFileSync('./txt/output.txt', textOut);
+// console.log('File written!')
+
+// 不阻塞、异步方式
+// fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
+//   if (err) { console.log('文件不存在') }
+
+//   fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
+//     console.log(data2)
+
+//     fs.readFile('./txt/append.txt', 'utf-8', (err, data3) => {
+//       console.log(data3);
+
+//       fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', err => {
+//         console.log('文件已写完')
+//       })
+//     })
+//   })
+// })
+
+// ----------------- 服务API -----------------
+// 创建一个服务
+const server = http.createServer((req, res) => {
+  res.end('Hello from web server')
+})
+
+// 监听来自127.0.0.1本机，端口为8000的请求
+server.listen(8000, '127.0.0.1', () => {
+  console.log('Listening to requests on port 8000');
+})
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # nodejs-node
 学习Nodejs记录
+
+## P8 同步阻塞代码，异步不阻塞代码
+
+## P9 异步读写文件
+`readFile()` 和 `writeFile()`具体参数看文档
+
+## P10 创建简单的web服务
+- `http.createServer()`：创建一个web服务；  
+- `res.end()`：服务器向请求方发送消息；  
+- `server.linten()`：启动监听连接的服务器，接收端口、地址、回调函数。
