cf7cc0346ca3fac45adb29759ed18cebb6837d09
Dahu-0712 <419460476@qq.com>
2023-11-23 22:21:07 +0800

    feat：P16 module.exports

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const http = require('http')
 // const url = require('node:url')
+const replaceTemplate = require('./module/replaceTemplate.js')
 
 // ----------------- 文件API -----------------
 // 阻塞、同步方式
@@ -33,25 +34,8 @@ const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.h
 const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8')
 const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8')
 
-
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8')
 let dataObj = JSON.parse(data)
-
-// 把templateCard中占位符全部替换并返回替换后的结果
-const replaceTemplate = (temp, product) => {
-  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
-  output = output.replace(/{%IMAGE%}/g, product.image);
-  output = output.replace(/{%PRICE%}/g, product.price);
-  output = output.replace(/{%FROM%}/g, product.from);
-  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-  output = output.replace(/{%QUANTITY%}/g, product.quantity);
-  output = output.replace(/{%DESCRIPTION%}/g, product.description);
-  output = output.replace(/{%ID%}/g, product.id);
-
-  if (!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
-  return output
-}
-
 
 // 创建一个服务
 const server = http.createServer((req, res) => {
--- README.md
+++ README.md
@@ -22,7 +22,7 @@
 1. 读取文件template文件夹下`.html`文件，以字符串形式返回；
 2. 遍历循环`data.json`中数据，通过字符串替换形式将替换`tempCard`的占位符。
 3. 将`templateOver`文件下占位符用`tempCard`替换。
-4. `res.end()`，启动服务，看效果。
+4. `res.end()`，启动服务，看效果。
 
 ## P15 从URL中读取参数
 引入`url`模块，版本原因，原视频中`url.parse`方法在我当前的v16.16版本已经废除，使用新语法`new URL`，可以不用引入`url`模块。  
@@ -31,3 +31,6 @@
 > 分配给 protocol 属性的无效的网址协议值将被忽略.
 
 最后继续使用字符串替换的方法，将找到的`data[id]`替换`templateProduct`中的占位符。
+
+## P16 使用module.exports
+将`replaceTemplate()`封装成公用方法，通过modules.exports导出。
