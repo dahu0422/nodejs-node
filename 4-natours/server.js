83f4852cfec37e0016a637b7753280e098d81f51
Dahu-0712 <419460476@qq.com>
2023-12-13 22:26:07 +0800

    feat：P83 创建一个Tour模型

--- 4-natours/server.js
+++ 4-natours/server.js
@@ -1,4 +1,5 @@
-const mongoose = require('mongodb');
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
 require('dotenv').config({ path: './config.env' });
 // console.log(process.env)
 
@@ -8,17 +9,28 @@ const DB = process.env.DATABASE.replace(
   process.env.DATABASE_PASSWORD,
 );
 
-mongoose
-  .connect(DB, {
-    useNewUrlParser: true,
-    // useCreateIndex: true,
-    // useFindAndModify: false,
-    useUnifiedTopology: true,
-  })
-  .then((con) => {
-    console.log(con);
-    console.log('DB connection successful');
-  });
+mongoose.connect(DB).then((con) => {
+  // console.log(con.connections);
+  console.log('DB connection successful');
+});
+
+const tourSchema = new Schema({
+  name: {
+    type: String,
+    required: [true, 'A toure must have a name'],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price'],
+  },
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
