e10a7d48fbe9bf709351a0a7c3cc760dca62a273
Dahu-0712 <419460476@qq.com>
2023-12-14 21:54:39 +0800

    feat：P90 调用model.findByIdAndDelete()删除数据库数据

--- 4-natours/controller/tourController.js
+++ 4-natours/controller/tourController.js
@@ -81,10 +81,17 @@ exports.updateTour = async (req, res) => {
 };
 
 // 删除某一条数据
-exports.deleteTour = (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    requestedAt: req.requestTime,
-    // data: { tours },
-  });
+exports.deleteTour = async (req, res) => {
+  try {
+    await Tour.findByIdAndDelete(req.params.id);
+    res.status(200).json({
+      status: 'success',
+      data: null,
+    });
+  } catch (error) {
+    req.status(400).json({
+      status: 'fail',
+      message: error,
+    });
+  }
 };
