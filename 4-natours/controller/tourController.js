2ed8804296a251cf9000c3e701c5e3f90fed963a
Dahu-0712 <419460476@qq.com>
2023-12-16 15:43:00 +0800

    feat：P96 query.select限制字段返回

--- 4-natours/controller/tourController.js
+++ 4-natours/controller/tourController.js
@@ -25,12 +25,20 @@ exports.getAllTours = async (req, res) => {
     queryStr = queryStr.replace(/\b(gte|gt|lt|lte)\b/g, (match) => `$${match}`);
     let query = Tour.find(JSON.parse(queryStr));
 
-    // 2) sort
+    // 2) sorting
     if (req.query.sort) {
       const sortBy = req.query.sort.split(',').join(' ');
       query = query.sort(sortBy);
     } else {
       query = query.sort('-createdAt');
+    }
+
+    // 3) Field limiting
+    if (req.query.fields) {
+      const fields = req.query.fields.split(',').join(' ');
+      query = query.select(fields);
+    } else {
+      query = query.select('-__v');
     }
 
     // EXECUTE QUERY
