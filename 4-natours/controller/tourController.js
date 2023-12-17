c1f08e52e4bf5aef8c604b691308c99f5208fe9f
Dahu-0712 <419460476@qq.com>
2023-12-17 16:41:32 +0800

    refactor：P99 重构查询逻辑，封装API Class。

--- 4-natours/controller/tourController.js
+++ 4-natours/controller/tourController.js
@@ -1,3 +1,4 @@
+const APIFeatures = require('../utils/apiFeatures');
 const Tour = require('./../models/tourModel');
 
 // 获取Top5的旅游数据
@@ -11,48 +12,13 @@ exports.aliasTopTours = (req, res, next) => {
 // 获取所有旅游数据
 exports.getAllTours = async (req, res) => {
   try {
-    // BUILD QUERY
-    // 1A) filtering
-    const queryObj = { ...req.query };
-    const excludedFields = ['page', 'limit', 'sort', 'fields'];
-    excludedFields.forEach((el) => delete queryObj[el]);
-
-    // 1B) advanced filtering
-    let queryStr = JSON.stringify(queryObj);
-    // 替换所有 gte、gt、lt、lte
-    queryStr = queryStr.replace(/\b(gte|gt|lt|lte)\b/g, (match) => `$${match}`);
-    let query = Tour.find(JSON.parse(queryStr));
-
-    // 2) sorting
-    if (req.query.sort) {
-      const sortBy = req.query.sort.split(',').join(' ');
-      query = query.sort(sortBy);
-    } else {
-      query = query.sort('-createdAt');
-    }
-
-    // 3) Field limiting
-    if (req.query.fields) {
-      const fields = req.query.fields.split(',').join(' ');
-      query = query.select(fields);
-    } else {
-      query = query.select('-__v');
-    }
-
-    // 4) pagination
-    const page = req.query.page || 1;
-    const limit = req.query.limit || 100;
-    const skip = (page - 1) * limit;
-
-    if (req.query.page) {
-      const numTours = await Tour.countDocuments(); // 查询collection下数据总条数
-      if (skip >= numTours) throw new Error('This page does not exit');
-    }
-
-    query = query.skip(skip).limit(limit);
-
     // EXECUTE QUERY
-    const tours = await query;
+    const features = new APIFeatures(Tour.find(), req.query)
+      .filter()
+      .sort()
+      .limitFiedlds()
+      .pagination();
+    const tours = await features.query;
 
     //SEND RESPONSE
     res.status(200).json({
---
+++ 4-natours/utils/apiFeatures.js
@@ -0,0 +1,49 @@
+class APIFeatures {
+  // mongoose中的query，express中的req.query
+  constructor(query, queryString) {
+    this.query = query;
+    this.queryString = queryString;
+  }
+
+  filter() {
+    const queryObj = { ...this.queryString };
+    const excludedFields = ['page', 'limit', 'sort', 'fields'];
+    excludedFields.forEach((el) => delete queryObj[el]);
+
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lt|lte)\b/g, (match) => `$${match}`); // 替换所有 gte、gt、lt、lte
+
+    this.query = this.query.find(JSON.parse(queryStr));
+    return this;
+  }
+
+  sort() {
+    if (this.queryString.sort) {
+      const sortBy = this.queryString.sort.split(',').join(' ');
+      this.query = this.query.sort(sortBy);
+    } else {
+      this.query = this.query.sort('-createdAt');
+    }
+    return this;
+  }
+
+  limitFiedlds() {
+    if (this.queryString.fields) {
+      const fields = this.queryString.fields.split(',').join(' ');
+      this.query = this.query.select(fields);
+    } else {
+      this.query = this.query.select('-__v');
+    }
+    return this;
+  }
+
+  pagination() {
+    const page = this.queryString.page || 1;
+    const limit = this.queryString.limit || 100;
+    const skip = (page - 1) * limit;
+    this.query = this.query.skip(skip).limit(limit);
+    return this;
+  }
+}
+
+module.exports = APIFeatures;
