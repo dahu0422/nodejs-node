cc50163c087b598fa11c6003b646ddbebe421975
Dahu-0712 <419460476@qq.com>
2023-12-19 23:04:40 +0800

    feat：Document Middleware 中间件，可以在保存到数据库前、后操作数据。

--- 4-natours/models/tourModel.js
+++ 4-natours/models/tourModel.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
+const slugify = require('slugify');
+
 const tourSchema = new Schema(
   {
     name: {
@@ -87,6 +89,22 @@ const tourSchema = new Schema(
 // 虚拟属性，查询该字段无效，仅用于业务处理对数据库中原有字段操作。
 tourSchema.virtual('durationWeeks').get(function () {
   return this.duration / 7;
+});
+
+// // DOCUMENT MIDDLEWARE：run before .save() and .create()
+tourSchema.pre('save', function (next) {
+  this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+tourSchema.pre('save', function (next) {
+  console.log('Will save document');
+  next();
+});
+
+tourSchema.post('save', function (doc, next) {
+  console.log(doc);
+  next();
 });
 
 const Tour = mongoose.model('Tour', tourSchema);
