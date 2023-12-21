7e89d7e26384cb05c81dbb7aac245e0cd75d0335
Dahu-0712 <419460476@qq.com>
2023-12-21 22:18:48 +0800

    feat：P110 匹配无效路由，返回错误提示信息。

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -7,16 +7,12 @@ const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
 // 1.MiddleWare
-console.log(process.env.NODE_ENV);
+// console.log(process.env.NODE_ENV);
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
-app.use((req, res, next) => {
-  console.log('Hello from the middleware');
-  next();
-});
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
@@ -25,5 +21,13 @@ app.use((req, res, next) => {
 // 2.Routes
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'fail',
+    message: `Can't find ${req.originalUrl} in this server`,
+  });
+  next();
+});
 
 module.exports = app;
