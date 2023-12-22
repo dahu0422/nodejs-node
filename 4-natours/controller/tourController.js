e457fd14005655f52af626c89fd46c1c79ac022d
Dahu-0712 <419460476@qq.com>
2023-12-22 21:08:15 +0800

    feat：P114 catch error async。提取了公共错误异常处理，摆脱了try catch逻辑。

--- 4-natours/controller/tourController.js
+++ 4-natours/controller/tourController.js
@@ -1,4 +1,5 @@
 const APIFeatures = require('../utils/apiFeatures');
+const catchAsync = require('../utils/catchAsync');
 const Tour = require('./../models/tourModel');
 
 // 获取Top5的旅游数据
@@ -10,177 +11,128 @@ exports.aliasTopTours = (req, res, next) => {
 };
 
 // 获取所有旅游数据
-exports.getAllTours = async (req, res) => {
-  try {
-    // EXECUTE QUERY
-    const features = new APIFeatures(Tour.find(), req.query)
-      .filter()
-      .sort()
-      .limitFiedlds()
-      .pagination();
-    const tours = await features.query;
+exports.getAllTours = catchAsync(async (req, res, next) => {
+  // EXECUTE QUERY
+  const features = new APIFeatures(Tour.find(), req.query)
+    .filter()
+    .sort()
+    .limitFiedlds()
+    .pagination();
+  const tours = await features.query;
 
-    //SEND RESPONSE
-    res.status(200).json({
-      status: 'success',
-      resule: tours.length,
-      data: { tours },
-    });
-  } catch (error) {
-    res.status(400).json({
-      status: 'fail',
-      message: error,
-    });
-  }
-};
+  //SEND RESPONSE
+  res.status(200).json({
+    status: 'success',
+    resule: tours.length,
+    data: { tours },
+  });
+});
 
 // 创建一条旅游数据
-exports.createTour = async (req, res) => {
-  try {
-    const newTour = await Tour.create(req.body);
-    res.status(200).json({
-      status: 'success',
-      data: { newTour },
-    });
-  } catch (error) {
-    res.status(400).json({
-      status: 'fail',
-      message: 'Invaild data send!',
-    });
-  }
-};
+exports.createTour = catchAsync(async (req, res, next) => {
+  const newTour = await Tour.create(req.body);
+  res.status(200).json({
+    status: 'success',
+    data: { newTour },
+  });
+});
 
 // 查询某一条旅游数据
-exports.getTour = async (req, res) => {
-  try {
-    const tour = await Tour.findById(req.params.id); // 根据id查找
-    // Tour.findOne({_id: req.params.id})
+exports.getTour = catchAsync(async (req, res, next) => {
+  const tour = await Tour.findById(req.params.id); // 根据id查找
+  // Tour.findOne({_id: req.params.id})
 
-    res.status(200).json({
-      status: 'success',
-      data: { tour },
-    });
-  } catch (error) {
-    res.status(400).json({
-      status: 'fail',
-      message: error,
-    });
-  }
-};
+  res.status(200).json({
+    status: 'success',
+    data: { tour },
+  });
+});
 
 // 修改某一条旅游数据
-exports.updateTour = async (req, res) => {
-  try {
-    // 找到并修改这条数据，返回修改后的对象
-    const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true,
-    });
-    res.status(200).json({
-      status: 'success',
-      data: { tour },
-    });
-  } catch (error) {
-    res.status(400).json({
-      status: 'fail',
-      error: error,
-    });
-  }
-};
+exports.updateTour = catchAsync(async (req, res, next) => {
+  // 找到并修改这条数据，返回修改后的对象
+  const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+  res.status(200).json({
+    status: 'success',
+    data: { tour },
+  });
+});
 
 // 删除某一条数据
-exports.deleteTour = async (req, res) => {
-  try {
-    await Tour.findByIdAndDelete(req.params.id);
-    res.status(200).json({
-      status: 'success',
-      data: null,
-    });
-  } catch (error) {
-    req.status(400).json({
-      status: 'fail',
-      message: error,
-    });
-  }
-};
+exports.deleteTour = catchAsync(async (req, res, next) => {
+  await Tour.findByIdAndDelete(req.params.id);
+  res.status(200).json({
+    status: 'success',
+    data: null,
+  });
+});
 
 // 获取统计数据
-exports.getTourStats = async (req, res) => {
-  try {
-    const stats = await Tour.aggregate([
-      { $match: { ratingsAverage: { $gte: 4.5 } } }, // 过滤平均分大于等于4.5的数据，并传给$group，group会将他们分组。
-      {
-        $group: {
-          _id: { $toUpper: '$difficulty' }, // 按照困难程度进行分组
-          numTours: { $sum: 1 },
-          numRatings: { $sum: 'ratingsQuantity' },
-          avgRating: { $avg: '$ratingsAverage' },
-          avgPrice: { $avg: '$price' },
-          minPrice: { $min: '$price' },
-          maxPrice: { $max: '$price' },
-        },
+exports.getTourStats = catchAsync(async (req, res, next) => {
+  const stats = await Tour.aggregate([
+    { $match: { ratingsAverage: { $gte: 4.5 } } }, // 过滤平均分大于等于4.5的数据，并传给$group，group会将他们分组。
+    {
+      $group: {
+        _id: { $toUpper: '$difficulty' }, // 按照困难程度进行分组
+        numTours: { $sum: 1 },
+        numRatings: { $sum: 'ratingsQuantity' },
+        avgRating: { $avg: '$ratingsAverage' },
+        avgPrice: { $avg: '$price' },
+        minPrice: { $min: '$price' },
+        maxPrice: { $max: '$price' },
       },
-      { $sort: { avgPrice: 1 } }, // 按照平均价降序排列
-      // { $match: { _id: { $ne: 'EASY' } } }, // 排除EASY的
-    ]);
-    res.status(200).json({
-      status: 'success',
-      data: {
-        stats,
-      },
-    });
-  } catch (error) {
-    res.status(400).send({
-      status: 'fail',
-      message: error,
-    });
-  }
-};
+    },
+    { $sort: { avgPrice: 1 } }, // 按照平均价降序排列
+    // { $match: { _id: { $ne: 'EASY' } } }, // 排除EASY的
+  ]);
+  res.status(200).json({
+    status: 'success',
+    data: {
+      stats,
+    },
+  });
+});
 
 // 获取年度每个月开展的旅游数据
-exports.getMountlyPlan = async (req, res) => {
-  try {
-    const year = req.params.year * 1;
+exports.getMountlyPlan = catchAsync(async (req, res, next) => {
+  const year = req.params.year * 1;
 
-    const plan = await Tour.aggregate([
-      { $unwind: '$startDates' }, // 解构startDates字段
-      // 查询日期在21-01-01 12-31之间的数据
-      {
-        $match: {
-          startDates: {
-            $gte: new Date(`${year}-01-01`),
-            $lte: new Date(`${year}-12-31`),
-          },
+  const plan = await Tour.aggregate([
+    { $unwind: '$startDates' }, // 解构startDates字段
+    // 查询日期在21-01-01 12-31之间的数据
+    {
+      $match: {
+        startDates: {
+          $gte: new Date(`${year}-01-01`),
+          $lte: new Date(`${year}-12-31`),
         },
       },
-      // 按照月份分组，统计每个月有多少活动，以及他们的名字分别是什么
-      {
-        $group: {
-          _id: { $month: '$startDates' },
-          numTourStarts: { $sum: 1 },
-          tours: { $push: '$name' },
-        },
+    },
+    // 按照月份分组，统计每个月有多少活动，以及他们的名字分别是什么
+    {
+      $group: {
+        _id: { $month: '$startDates' },
+        numTourStarts: { $sum: 1 },
+        tours: { $push: '$name' },
       },
-      // 添加字段
-      { $addFields: { month: '$_id' } },
-      // 移除字段
-      { $project: { _id: 0 } },
-      // 按照数量降序排列
-      { $sort: { numTourStarts: -1 } },
-      // 限制返回个数
-      { $limit: 12 },
-    ]);
+    },
+    // 添加字段
+    { $addFields: { month: '$_id' } },
+    // 移除字段
+    { $project: { _id: 0 } },
+    // 按照数量降序排列
+    { $sort: { numTourStarts: -1 } },
+    // 限制返回个数
+    { $limit: 12 },
+  ]);
 
-    res.status(200).json({
-      status: 'success',
-      data: {
-        plan,
-      },
-    });
-  } catch (error) {
-    res.status(400).send({
-      status: 'fail',
-      message: error,
-    });
-  }
-};
+  res.status(200).json({
+    status: 'success',
+    data: {
+      plan,
+    },
+  });
+});
