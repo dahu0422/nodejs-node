850af9dcb572e66f3d7f864d355736bfe84c348a
Dahu-0712 <419460476@qq.com>
2023-12-23 15:49:36 +0800

    feat：P118、119 处理唯一值字段重复错误、添加字段不符合校验规则的错误。

--- 4-natours/controller/errorController.js
+++ 4-natours/controller/errorController.js
@@ -1,3 +1,25 @@
+const AppError = require('../utils/appError');
+
+// 处理查询数据异常情况，mongoose返回CastError
+const handleCastErrorDB = (err) => {
+  const message = `Invalid ${err.path}: ${err.value}`;
+  return new AppError(message, 400);
+};
+
+// 处理添加重复字段问题，mongodb返回code为11000
+const handleDuplicateFieldsDB = (err) => {
+  const value = err.keyValue.name;
+  const message = `Duplicate field value: ${value}. Please use another value!`;
+  return new AppError(message, 400);
+};
+
+// 处理mongoose校验错误，mongoose返回ValidationError
+const handleValidationErrorDB = (err) => {
+  const errors = Object.values(err.errors).map((el) => el.message);
+  const message = `Invalid input data ${errors.join('. ')}`;
+  return new AppError(message, 400);
+};
+
 // 开发模式下的错误处理
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
@@ -24,11 +46,19 @@ const sendErrorProd = (err, res) => {
 };
 
 module.exports = (err, req, res, next) => {
+  console.log(err);
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
   if (process.env.NODE_ENV === 'development') {
     sendErrorDev(err, res);
   } else if (process.env.NODE_ENV === 'production') {
-    sendErrorProd(err, res);
+    let error = JSON.parse(JSON.stringify(err)); // 无论使用哪种方式，这里拿到的error和传进来的err有些区别。
+
+    if (error.name === 'CastError') error = handleCastErrorDB(error);
+    if (error.code === 11000) error = handleDuplicateFieldsDB(error);
+    if (error.name === 'ValidationError')
+      error = handleValidationErrorDB(error);
+
+    sendErrorProd(error, res);
   }
 };
