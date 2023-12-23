64b6c40830d907fc84cfd1a5e70b3a83148ddeb7
Dahu-0712 <419460476@qq.com>
2023-12-23 16:18:36 +0800

    feat：P120、121 捕获unhandledRejection、uncaughtException错误

--- 4-natours/server.js
+++ 4-natours/server.js
@@ -1,6 +1,11 @@
 const mongoose = require('mongoose');
 require('dotenv').config({ path: './config.env' });
-// console.log(process.env)
+
+process.on('uncaughtException', (err) => {
+  console.log('UNCAUGHT EXCEPTION! 💥 Shutting down...');
+  console.log(err.name, err.message);
+  process.exit(1);
+});
 
 const app = require('./app');
 const DB = process.env.DATABASE.replace(
@@ -16,4 +21,12 @@ mongoose.connect(DB).then((con) => {
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App runing on port ${port}`);
+});
+
+process.on('unhandledRejection', (err) => {
+  console.log('UNHANDLED REJECTION! 💥 Shutting down...');
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
 });
