44a18082e5660a5dc353be76d549dca6cb4ca8a4
Dahu-0712 <419460476@qq.com>
2023-12-23 14:12:20 +0800

    feat：P116 区分生产环境、开发环境错误处理

--- 4-natours/controller/errorController.js
+++ 4-natours/controller/errorController.js
@@ -1,8 +1,34 @@
+// 开发模式下的错误处理
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+    stack: err.stack,
+    error: err,
+  });
+};
+
+// 生产模式下的错误处理
+const sendErrorProd = (err, res) => {
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+  } else {
+    res.status(500).json({
+      status: 'error',
+      message: 'Something went very wrong',
+    });
+  }
+};
+
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
-  res.status(err.statusCode).json({
-    status: err.status,
-    statusCode: err.statusCode,
-  });
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    sendErrorProd(err, res);
+  }
 };
