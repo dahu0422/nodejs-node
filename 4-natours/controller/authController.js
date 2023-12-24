903466ae1dfce945211dfff6647e66ccdf6c881b
Dahu-0712 <419460476@qq.com>
2023-12-24 16:27:42 +0800

    feat：P125 bcryptjs第三方插件，对密码加密。校验二次输入密码是否与原始密码相同。

--- 4-natours/controller/authController.js
+++ 4-natours/controller/authController.js
@@ -2,7 +2,6 @@ const User = require('../models/userModel.js');
 const catchAsync = require('../utils/catchAsync.js');
 
 exports.signup = catchAsync(async (req, res, next) => {
-  console.log(req.body);
   const newUser = await User.create(req.body);
 
   res.status(201).json({
--- 4-natours/models/userModel.js
+++ 4-natours/models/userModel.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
+const bcrypt = require('bcryptjs');
 const validator = require('validator');
 
 // name, email, photo, password, passwordConfirm
@@ -26,7 +27,26 @@ const userSchema = new Schema({
   passwordConfirm: {
     type: String,
     required: [true, 'Please confirm your password'],
+    // This only works on CREATE and SAVE
+    validate: {
+      validator: function (el) {
+        return this.password === el;
+      },
+      message: 'Passwords are not the same',
+    },
   },
+});
+
+userSchema.pre('save', async function (next) {
+  // Only run this function if password was actually modified
+  if (!this.isModified('password')) return next();
+
+  // Hash the password with cost of 12
+  this.password = await bcrypt.hash(this.password, 12);
+
+  // Delete passwordConfirm field
+  this.passwordConfirm = undefined;
+  next();
 });
 
 const User = mongoose.model('User', userSchema);
